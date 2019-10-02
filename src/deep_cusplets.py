910870d597f4991896178f77395c326d562509f8
David Rushing Dewhurst <david.dewhurst@uvm.edu>
2019-10-02 15:06:24 -0400

    added power law cusplets (in addition to power cusplets) and added user-defined endpoints

--- src/deep_cusplets.py
+++ src/deep_cusplets.py
@@ -6,6 +6,8 @@ import pathlib
 import joblib
 import numpy as np
 from sklearn.neural_network import MLPRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import AdaBoostRegressor
 
 import cusplets
 import stat_tools
@@ -77,7 +79,8 @@ def classify_cusps(indic, geval=0.5):
 def predict(
         model,
         time_series_matrix,
-        feature_dim
+        feature_dim,
+        alg='en'
         ):
     time_series_matrix = np.array( time_series_matrix )
     if len( time_series_matrix.shape ) == 1:
@@ -88,6 +91,7 @@ def predict(
     X_vars, mean, std = stat_tools.row_normalize( X_vars )
     X_vars[np.isnan(X_vars)] = 0.
     X_vars = np.vstack( X_vars )
+
     return model.predict( X_vars )
 
 
@@ -104,11 +108,12 @@ def create_and_train_mlp_model(
     model = MLPRegressor( 
             hidden_layer_sizes=(int(data_dim / 2), int(data_dim / 4),),
             solver='adam',
-            max_iter=1000,
+            max_iter=500,
             verbose=verbose,
             alpha=0.0025,
             early_stopping=True,
-            tol=5e-4
+            tol=1e-4,
+            learning_rate='adaptive'
             )
     fit_model = model.fit(X_train, y_train)
     if savemodel:
@@ -123,3 +128,179 @@ def create_and_train_mlp_model(
             joblib.dump( fit_model, fname, compress=3 )
             print(f'saved model at {fname}')
     return fit_model
+
+
+def create_and_train_knn_model(
+        X_train,
+        y_train,
+        verbose=True,
+        savemodel=True,
+        savedir='./models',
+        savename='mlp_model',
+        overwrite=True
+        ):
+    data_dim = X_train.shape[1]
+    model = KNeighborsRegressor( 
+            n_jobs=-1
+            )
+    fit_model = model.fit(X_train, y_train)
+    if savemodel:
+        pathlib.Path( savedir ).mkdir(
+            exist_ok=True,
+            parents=True
+            )
+        fname = savedir + '/' + savename + '.joblib.lzma'
+        if os.path.exists( fname ) and not overwrite:
+            print(f'Not saving model since {fname} exists already')
+        else:
+            joblib.dump( fit_model, fname, compress=3 )
+            print(f'saved model at {fname}')
+    return fit_model
+
+
+def create_and_train_adaboost_model(
+        X_train,
+        y_train,
+        verbose=True,
+        savemodel=True,
+        savedir='./models',
+        savename='adaboost_model',
+        overwrite=True
+        ):
+    data_dim = X_train.shape[1]
+    model = AdaBoostRegressor( 
+            loss='square'
+            )
+    fit_model = model.fit(X_train, y_train)
+    if savemodel:
+        pathlib.Path( savedir ).mkdir(
+            exist_ok=True,
+            parents=True
+            )
+        fname = savedir + '/' + savename + '.joblib.lzma'
+        if os.path.exists( fname ) and not overwrite:
+            print(f'Not saving model since {fname} exists already')
+        else:
+            joblib.dump( fit_model, fname, compress=3 )
+            print(f'saved model at {fname}')
+    return fit_model
+
+
+def create_and_train_en_model(
+        X_train,
+        y_train,
+        verbose=True,
+        savemodel=True,
+        savedir='./models',
+        savename='en',
+        overwrite=True
+        ):
+    from sklearn.linear_model import ElasticNetCV
+    data_dim = X_train.shape[1]
+    model = ElasticNetCV( 
+            verbose=True,
+            n_jobs=-1,
+            selection='random',
+            max_iter=3000,
+            l1_ratio=np.linspace(0.05, 1, 10),
+            cv=3
+            )
+    fit_model = model.fit(X_train, y_train)
+    if savemodel:
+        pathlib.Path( savedir ).mkdir(
+            exist_ok=True,
+            parents=True
+            )
+        fname = savedir + '/' + savename + '.joblib.lzma'
+        if os.path.exists( fname ) and not overwrite:
+            print(f'Not saving model since {fname} exists already')
+        else:
+            joblib.dump( fit_model, fname, compress=3 )
+            print(f'saved model at {fname}')
+    return fit_model
+
+
+def create_and_train_sgd_model(
+        X_train,
+        y_train,
+        verbose=True,
+        savemodel=True,
+        savedir='./models',
+        savename='sgd',
+        overwrite=True,
+        loss='huber'
+        ):
+    from sklearn.linear_model import SGDRegressor
+
+    data_dim = X_train.shape[1]
+    model = SGDRegressor(
+            loss=loss,
+            verbose=1,
+            penalty='elasticnet',
+            learning_rate='optimal',
+            max_iter=150,
+            early_stopping=True,
+             )
+    print('training svr with sgd')
+    fit_model = model.fit(X_train, y_train)
+    if savemodel:
+        pathlib.Path( savedir ).mkdir(
+            exist_ok=True,
+            parents=True
+            )
+        fname = savedir + '/' + savename + '.joblib.lzma'
+        if os.path.exists( fname ) and not overwrite:
+            print(f'Not saving model since {fname} exists already')
+        else:
+            joblib.dump( fit_model, fname, compress=3 )
+            print(f'saved model at {fname}')
+    return fit_model
+
+
+def create_and_train_model(
+        X_train,
+        y_train,
+        verbose=True,
+        savemodel=True,
+        savedir='./models',
+        savename='mlp_model',
+        overwrite=True
+        ):
+    import keras
+
+    X_train = X_train[..., np.newaxis]
+
+    model = keras.models.Sequential([ 
+            keras.layers.Conv1D(32, 3, activation='relu',
+                input_shape=(X_train.shape[1], 1)),
+            keras.layers.Conv1D(32, 3, activation='relu'),
+            keras.layers.MaxPooling1D(3),
+            keras.layers.BatchNormalization(),
+            keras.layers.Conv1D(64, 3, activation='relu'),
+            keras.layers.Conv1D(64, 3, activation='relu'),
+            keras.layers.GlobalAveragePooling1D(),
+            keras.layers.BatchNormalization(),
+            keras.layers.Dropout(0.4),
+            keras.layers.Dense(1, activation='linear'),
+            ])
+
+    model.compile(
+            optimizer='adam',
+            loss='mse',
+            metrics=['accuracy']
+            )
+    model.fit(X_train, y_train, epochs=20, batch_size=512, validation_split=0.2)
+
+    if savemodel:
+        pathlib.Path( savedir ).mkdir(
+            exist_ok=True,
+            parents=True
+            )
+        fname = savedir + '/' + savename + '.joblib.lzma'
+        if os.path.exists( fname ) and not overwrite:
+            print(f'Not saving model since {fname} exists already')
+        else:
+            joblib.dump( model, fname, compress=3 )
+            print(f'saved model at {fname}')
+    return model
+
--- src/run_deep_cusplets_test.py
+++ src/run_deep_cusplets_test.py
@@ -13,63 +13,15 @@ import deep_cusplets
 from shape_detector import word_idx
 
 
-def train_test_dow30():
-    
-    dow30 = pd.read_csv( '../../../../finance-data/dow30_data/data/dow30-19900101-20180202.csv' )
-
-    tickers = sorted(list(set(dow30.ticker)))
-    ts = [dow30[dow30.ticker == ticker].Volume.values for ticker in tickers]
-    ts = np.array( [x for x in ts if len(x) == 7079 ] )  # that's the max length
-    feature_dim = 750
-    test_ind = 5000
-    wmin = 10
-    wmax = 750
-    nw = 300
-    
-    Xtrain, ytrain, Xtest, ytest = deep_cusplets.make_indic_regression_data( 
-            ts[:, :test_ind],
-            feature_dim=feature_dim,
-            wmin=wmin,
-            wmax=wmax,
-            nw=nw,
-            k_args=[3.],
-            train_frac=1.0
-            )
-    fit_model = deep_cusplets.create_and_train_mlp_model(Xtrain, ytrain, savename='traindow_testdow_mlp_fd700')
-
-    # plot all the test data
-    fig_len = np.ceil( np.sqrt(ts.shape[0]) ).astype(int)
-    fig, axes = plt.subplots(fig_len, fig_len, figsize=(10, 10) )
-    axes = axes.flatten()
-
-    for i in range(len(ts)):
-
-        test_ts = ts[i, test_ind - feature_dim:]  # leave on feature_dim since need it to predict first output
-        
-        preds = deep_cusplets.predict( fit_model, test_ts, feature_dim )
-        # make the true indicator for comparison
-        cc, _ = cusplets.cusplet( 
-                test_ts,
-                cusplets.power_cusp,
-                np.linspace(wmin, wmax, nw).astype(int),
-                k_args=[3.]
-                )
-        cusps, cusp_indic, gecusp = cusplets.classify_cusps(cc, b=0.75, geval=0.5)
-
-        axes[i].plot(cusp_indic[feature_dim:], color='k', linewidth=2)
-        axes[i].plot(preds, linewidth=0.5, color='r')
-
-    plt.savefig( f'./deep_cusplets_figures/traindow_testdow.png' )
-
-
-def train_test_labmt(load=False):
+def train_test_labmt(load=False, alg='elastic_net'):
+    np.random.seed( 1 )
     # we were provided with a matlab-style file.....
     mat = io.loadmat('../_data/rank_turbulence_extractorify006_spearman.mat')
     if not os.path.exists( '../_data/labmt_ranks.npz' ):
         print('creating the data')
         ranks = []
         for i, word in enumerate(mat['ok_words']):
-            word = word[0][0] # fucking matlab
+            word = word[0][0]
             word_ranks = mat['ok_wordranks'][word_idx(word, mat['ok_words']), :].T
             word_ranks = -np.squeeze(word_ranks)
             ranks.append(word_ranks)
@@ -81,7 +33,7 @@ def train_test_labmt(load=False):
         print('loading the data')
         ranks = np.load('../_data/labmt_ranks.npz')['ranks']
 
-    rand_inds = np.random.choice( range(ranks.shape[0]), size=200, replace=False )
+    rand_inds = np.random.choice( range(ranks.shape[0]), size=100, replace=False )
 
     feature_dim = 750
     test_ind = 2000
@@ -99,23 +51,34 @@ def train_test_labmt(load=False):
                 k_args=[3.],
                 train_frac=1.
                 )
-
-        fit_model = deep_cusplets.create_and_train_mlp_model(Xtrain, ytrain, savename='trainlabmt_testlabmt_mlp_fd750')
+        
+        if alg == 'elastic_net':
+            fit_model = deep_cusplets.create_and_train_en_model(Xtrain, ytrain, savename='trainlabmt_testlabmt_en')
+        elif alg == 'sgd':
+            loss = 'squared_loss'
+            fit_model = deep_cusplets.create_and_train_sgd_model(Xtrain, ytrain,
+                    savename=f'trainlabmt_testlabmt_sgd{loss}',
+                    loss=loss)
 
     else:
-        fit_model = joblib.load( './models/trainlabmt_testlabmt_mlp_fd750.joblib.lzma' )
+        if alg == 'elastic_net':
+            fit_model = joblib.load( './models/trainlabmt_testlabmt_en.joblib.lzma' )
+        elif alg == 'sgd':
+            loss = 'huber'
+            fit_model = joblib.load( f'./models/trainlabmt_testlabmt_sgd{loss}.joblib.lzma' )
+
 
     # show results on some random words
     test_inds = np.random.choice( [x for x in np.linspace(0, ranks.shape[0] - 1, ranks.shape[0]).astype(int)\
             if x not in rand_inds],
-            size=144, replace=False)
+            size=100, replace=False)
 
-    fig, axes = plt.subplots(12, 12, figsize=(20, 20))
+    fig, axes = plt.subplots(10, 10, figsize=(20, 20))
     axes = axes.flatten()
     for i, (ax, ind) in enumerate( zip(axes, test_inds) ):
 
-        testrank = ranks[ind]        
-        preds = deep_cusplets.predict( fit_model, testrank, feature_dim )
+        testrank = ranks[ind]
+        preds = deep_cusplets.predict( fit_model, testrank, feature_dim, alg=alg )
 
         # make the true indicator for comparison
         cc, _ = cusplets.cusplet( 
@@ -126,12 +89,40 @@ def train_test_labmt(load=False):
                 )
         cusps, cusp_indic, gecusp = cusplets.classify_cusps(cc, b=0.75, geval=0.5)
 
-        axes[i].plot(cusp_indic[feature_dim:], color='k', linewidth=2)
+        axes[i].plot(cusp_indic[feature_dim:], color='b', linewidth=2)
         axes[i].plot(preds, linewidth=0.5, color='r')
         axes[i].set_title(mat['ok_words'][ind][0][0])
+        axes[i].set_xticks([])
+        axes[i].set_yticks([])
+        axes[i].set_xticklabels([])
+        axes[i].set_yticklabels([])
     
     plt.tight_layout()
-    plt.savefig('./deep_cusplets_figures/trainlabmt_testlabmt.pdf')
+
+    if alg == 'elastic_net':
+        plt.savefig('./deep_cusplets_figures/trainlabmt_testlabmt_en.pdf')
+        plt.close()
+
+        # now viz the coefs
+        coef = fit_model.coef_
+        fig, ax = plt.subplots(figsize=(7, 5))
+        ax.plot(range(1, len(coef) + 1),
+                coef[::-1],
+                'k-')
+        ax.set_xlabel('$\\tau$ (lags)', fontsize=15)
+        ax.set_ylabel('$\\beta_{\\tau}$', fontsize=15)
+        ax.tick_params(labelsize=15)
+        ax.set_xlim( -10, len(coef) + 1 )
+
+        ax.axhline(0, 0, 1, color='k', linestyle='--', linewidth=0.5)
+
+        plt.tight_layout()
+
+        plt.savefig('./deep_cusplets_figures/trainlabmt_testlabmt_en_coefs.pdf')
+
+    elif alg == 'sgd':
+        plt.savefig(f'./deep_cusplets_figures/trainlabmt_testlabmt_sgd{loss}.pdf')
+        plt.close()
 
 
 def plot_traindow_testdow():
@@ -223,4 +214,4 @@ def train_labmt_test_dow30():
 
 if __name__ == "__main__":
     
-    train_test_labmt(load=False) 
+    train_test_labmt(load=False, alg='elastic_net') 
